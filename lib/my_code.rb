31d99d91b7d4243bfddc4451ee871bb0a66565fd
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-26 20:47:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,8 @@
 def map(array) 
   array.map { |ele| yield(ele)}
 end 
+
+def reduce(source_array, starting_point = 0)
+  source_array.each { |num| starting_point += num}
+  return starting_point
+end 
